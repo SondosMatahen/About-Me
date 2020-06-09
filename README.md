c62af89f03e16db4c2db6ac9af24909a9a8686f5
SondosMatahen <Sondosmatahen@gmail.com>
2020-06-09 16:15:13 +0300

    lab3

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ I am Sondos Matahen , Iam 23 years old. From Jordan.
 I studied civil engineering in JUST in Jordan.
 I worked as a civil engineer for three months.
 Now , I am learning coding in ASAC , also I'm in level 201 .
+for today we will play with user sach a game , with using diffrent type of loops
--- js/app.js
+++ js/app.js
@@ -2,11 +2,13 @@
 
 var userName = prompt('What\'s your name ?' )
 alert ( 'Welcom to our websit '+ userName)
+var score=0
 
 var genderG= prompt('Am I a Girl? \n yes/no').toLowerCase();
 if (genderG=='yes'){
     //console.log('You are right')
     alert('You are right')
+    score++;
     }
     else {
     //console.log('You are wrong')
@@ -16,6 +18,7 @@ if (genderG=='yes'){
 
 var age= prompt('Am I above 20 years? \n yes/no').toUpperCase();
 if (age=='YES'){
+    score++;
     //console.log('You are right')
     alert('You are right')
     }
@@ -27,6 +30,7 @@ else {
 
 var friend= prompt('Am I your friend?? \n yes/no').toUpperCase();
 if (friend=='YES'){
+    score++;
     //console.log('You are right')
     alert('You are right')
     }
@@ -37,6 +41,7 @@ else {
 
 var bea= prompt('Am I beautiful ?? \n yes/no').toUpperCase();
 if (bea=='YES'){
+    score++;
     //console.log('You are right')
     alert('You are right')
     }
@@ -47,17 +52,65 @@ else {
 
 
 var summer= prompt('Do I like summer?? \n yes/no').toUpperCase();
-if (summer=='NO'){
+if (summer=='NO'){score++;
     //console.log('You are right')
     alert('You are right')
     }
 else {
+    
      //console.log('You are wrong')
      alert('You are wrong')
     }
 
+
+var userInput=prompt ('This is guessing game , you should guess the number \n you just have 4 guesses ')
+var guess=7;
+for (var i=0 ; i<3 ; i++){
+
+     if(userInput>guess){
+        userInput= prompt('it\'s too high');}
+
+    
+    else if (userInput==guess){
+        alert('Wow It\'s correct');
+        score++;
+        break;
+    }  
+    else {
+        userInput= prompt('it\'s too low');
+        
+}
+}
+
+if(userInput!=guess){
+alert('Unfortunately you didn\'t guess it \n The answer was 7')}
+
+
+var userIn=prompt ('This is guessing game , you should guess the number from (1-15) \n you just have 6 guesses ')
+var arr=[7,4,14,1,3,8,9]
+var index = arr [0]
+var i=0
+
+while(i<5) {
+    
+    for (var y=0; y<arr.length;y++){
+    index= arr[y];
+    console.log(y)
+    if (userIn==index){
+        alert ('Wow It\'s correct')
+        break;
+    }
+    }
+    if (userIn==index){   score++
+        break;}
+    var userIn=prompt ( 'guess again')
+ i++ 
+ 
+} 
+if(userIn!=index){
+    alert('Unfortunately you didn\'t guess it \n The answers are 7,4,14,1,3,8,9')}
+    
+
+alert('your score is '+score)
+console.log('your score is '+score)
 alert('Thank You  '+ userName +'For Your Visiting')
-
-
-
-
